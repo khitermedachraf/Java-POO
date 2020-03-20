e0f905a996b18822dbb96b828856ccbd84fd5686
khitermedachraf <hm_khiter@esi.dz>
2020-03-20 19:43:35 +0100

    creat the first project Maven using java  pom.xml

--- Eclipse- Java/HellowWorld/src/com/openclassrooms/HelloWorld.java
+++ Eclipse- Java/HellowWorld/src/com/openclassrooms/HelloWorld.java
@@ -1,10 +1,16 @@
 package com.openclassrooms;
 
 public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("J'utilise Eclipse !i love you ");
-	}
-
+    
+    public static void main(String[] args) {
+        int i;
+		for (i = 0; i < 5; i++) {
+			System.out.println("Bienvenue dans Eclipse !");
+		}
+		
+		System.out.println("C'est la fin !");
+    }
+    public int sommeNombres(int num1,int num2) {
+    	return num1 + num2 ; 
+    }
 }
